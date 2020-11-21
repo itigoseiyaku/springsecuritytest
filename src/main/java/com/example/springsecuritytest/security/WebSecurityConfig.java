b2edbadeb71eb4e8ca398b211f9d824d515f4162
itigoseiyaku <itigoseiyaku@users.noreply.github.com>
2020-11-21 12:49:29 +0900

    not use formLogin()

--- src/main/java/com/example/springsecuritytest/security/WebSecurityConfig.java
+++ src/main/java/com/example/springsecuritytest/security/WebSecurityConfig.java
@@ -3,17 +3,24 @@ package com.example.springsecuritytest.security;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.authentication.SimpleUrlAuthenticationFailureHandler;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
@@ -25,20 +32,24 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     public void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
-                .antMatchers(HttpMethod.GET, "/login").denyAll()
-                .antMatchers(HttpMethod.POST, "/login").permitAll()
+                .antMatchers("/login").permitAll()
                 .anyRequest()
                 .authenticated()
                 .and()
-                .formLogin()
-                .defaultSuccessUrl("http://localhost:9090?result=ok")
-                .failureUrl("http://localhost:9090?result=ng")
+                .logout()
+//                .defaultSuccessUrl("http://localhost:9090?result=ok")
+//                .failureUrl("http://localhost:9090?result=ng")
                 .and()
                 .cors().configurationSource(this.corsConfigurationSource())
                 .and()
                 .csrf().disable();
 
-        http.exceptionHandling().accessDeniedPage("/accessDenied.html");
+                UsernamePasswordAuthenticationFilter filter = new UsernamePasswordAuthenticationFilter();
+                filter.setRequiresAuthenticationRequestMatcher(new AntPathRequestMatcher("/login", "POST"));
+                filter.setAuthenticationManager(authenticationManagerBean());
+                filter.setAuthenticationFailureHandler(new SimpleUrlAuthenticationFailureHandler("/login?error"));
+
+                http.addFilterBefore(filter, UsernamePasswordAuthenticationFilter.class);
     }
 
     private CorsConfigurationSource corsConfigurationSource() {
